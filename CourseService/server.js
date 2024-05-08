98ddfdd1b426038400168e9d2001d50afdb83301
Sandeepa123 <sandeeparathnayaka20@gmail.com>
2024-05-08 11:45:27 +0530

    courseBackendUpdated

--- CourseService/server.js
+++ CourseService/server.js
@@ -2,13 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
-import videoRoutes from "./routes/course.js";
+import courseRoute from "./routes/course.js";
 import signUploadRoutes from "./routes/sign-upload.js";
 import { errorHandler } from "./middlewares/error.js";
 
 dotenv.config();
 
-// Express App
 const app = express();
 const port = process.env.PORT || 8002;
 
@@ -17,14 +16,14 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/api/videos", videoRoutes);
+app.use("/api/videos", courseRoute);
 app.use("/api/sign-upload", signUploadRoutes);
 
 app.use(errorHandler);
 
 // Listen to the requests
 app.listen(port, () => {
-  // connect to DB
+  // connection of DB
   connectDB();
-  console.log("Server started listening on port", port);
+  console.log("Server is listening on port", port);
 });
